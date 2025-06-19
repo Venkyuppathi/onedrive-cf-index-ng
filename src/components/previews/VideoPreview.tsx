dba362a6276638ed4ee785c41d45d22d16092646
Venkyuppathi <91419699+Venkyuppathi@users.noreply.github.com>
2025-06-18 22:20:52 -0700

    Update VideoPreview.tsx

--- src/components/previews/VideoPreview.tsx
+++ src/components/previews/VideoPreview.tsx
@@ -1,37 +1,42 @@
-// components/VideoPreview.tsx
+'use client'
 
-'use client';
+import React from 'react'
+import dynamic from 'next/dynamic'
+import type { FC } from 'react'
+import type { OdFileObject } from '../../types'
 
-import dynamic from 'next/dynamic';
-import React from 'react';
-
-const Plyr = dynamic(() => import('plyr-react'), { ssr: false });
+// Dynamically import plyr-react (no SSR)
+const Plyr = dynamic(() => import('plyr-react'), { ssr: false })
 
 interface VideoPreviewProps {
-  videoUrl: string;
+  file: OdFileObject
 }
 
-const VideoPreview: React.FC<VideoPreviewProps> = ({ videoUrl }) => {
+const VideoPreview: FC<VideoPreviewProps> = ({ file }) => {
+  const videoUrl = `/api/raw?path=${encodeURIComponent(file.name)}`
+
   const plyrProps = {
     source: {
       type: 'video',
+      title: file.name,
       sources: [
         {
           src: videoUrl,
+          type: file?.mimeType || 'video/mp4',
           provider: 'html5',
         },
       ],
     },
     options: {
-      controls: ['play', 'progress', 'volume', 'fullscreen'],
+      controls: ['play', 'progress', 'current-time', 'mute', 'volume', 'settings', 'fullscreen'],
     },
-  };
+  }
 
   return (
-    <div className="video-wrapper">
+    <div className="video-wrapper px-2 py-4">
       <Plyr {...plyrProps} />
     </div>
-  );
-};
+  )
+}
 
-export default VideoPreview;
+export default VideoPreview
